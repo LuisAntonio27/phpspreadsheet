3d469a1716b4a728bcc11255c7261bcdf606597e
Luis Antonio <luisantonio27@live.com.mx>
2020-03-14 17:26:51 -0600

    cambio de variable

--- prueba2.php
+++ prueba2.php
@@ -26,100 +26,84 @@ $spreadsheet->getDefaultStyle()
 			->setSize(10);
 
 //set column dimension to auto size
-$spreadsheet->getActiveSheet()
-			->getColumnDimension('B')
-			->setAutoSize(true);
-$spreadsheet->getActiveSheet()
-			->getColumnDimension('C')
-			->setAutoSize(true);
+$sheet->getColumnDimension('B')
+		->setAutoSize(true);
+$sheet->getColumnDimension('C')
+		->setAutoSize(true);
 
 //simple text data
-$spreadsheet->getActiveSheet()
-			->setCellValue('A1',"String")
-			->setCellValue('B1',"Simple Text")
-			->setCellValue('C1',"This is Phpspreadsheet");
+$sheet->setCellValue('A1',"String")
+		->setCellValue('B1',"Simple Text")
+		->setCellValue('C1',"This is Phpspreadsheet");
 
 //symbols
-$spreadsheet->getActiveSheet()
-			->setCellValue('A2',"String")
-			->setCellValue('B2',"Symbols")
-			->setCellValue('C2',"ÚÔÛï¢£´°ƤǠњс҃ҭ");
+$sheet->setCellValue('A2',"String")
+		->setCellValue('B2',"Symbols")
+		->setCellValue('C2',"ÚÔÛï¢£´°ƤǠњс҃ҭ");
 
 //utf-8 string
-$spreadsheet->getActiveSheet()
-			->setCellValue('A3',"String")
-			->setCellValue('B3',"UTF-8")
-			->setCellValue('C3',"добро пожаловать в мой учебник видео");
+$sheet->setCellValue('A3',"String")
+		->setCellValue('B3',"UTF-8")
+		->setCellValue('C3',"добро пожаловать в мой учебник видео");
 
 
 //integer
-$spreadsheet->getActiveSheet()
-			->setCellValue('A4',"Number")
-			->setCellValue('B4',"Integer")
-			->setCellValue('C4',55);
+$sheet->setCellValue('A4',"Number")
+		->setCellValue('B4',"Integer")
+		->setCellValue('C4',55);
 
 //float
-$spreadsheet->getActiveSheet()
-			->setCellValue('A5',"Number")
-			->setCellValue('B5',"Float")
-			->setCellValue('C5',55.55);
+$sheet->setCellValue('A5',"Number")
+		->setCellValue('B5',"Float")
+		->setCellValue('C5',55.55);
 
 //negative
-$spreadsheet->getActiveSheet()
-			->setCellValue('A6',"Number")
-			->setCellValue('B6',"Negative")
-			->setCellValue('C6',-55.55);
+$sheet->setCellValue('A6',"Number")
+		->setCellValue('B6',"Negative")
+		->setCellValue('C6',-55.55);
 //boolean
-$spreadsheet->getActiveSheet()
-			->setCellValue('A7',"Number")
-			->setCellValue('B7',"Boolean")
-			->setCellValue('C7',true)
-			->setCellValue('D7',false);
+$sheet->setCellValue('A7',"Number")
+		->setCellValue('B7',"Boolean")
+		->setCellValue('C7',true)
+		->setCellValue('D7',false);
 
 //date datatype
 //make a variable from current timestamp
 $dateTimeNow = time();
 
 //date
-$spreadsheet->getActiveSheet()
-			->setCellValue('A8',"Date/Time")
-			->setCellValue('B8',"Date")
-			->setCellValue('C8',Date::PHPToExcel($dateTimeNow));
+$sheet->setCellValue('A8',"Date/Time")
+		->setCellValue('B8',"Date")
+		->setCellValue('C8',Date::PHPToExcel($dateTimeNow));
 
 //set the cell format into a date
-$spreadsheet->getActiveSheet()
-			->getStyle('C8')
-			->getNumberFormat()
-			->setFormatCode(NumberFormat::FORMAT_DATE_YYYYMMDD2);
+$sheet->getStyle('C8')
+		->getNumberFormat()
+		->setFormatCode(NumberFormat::FORMAT_DATE_YYYYMMDD2);
 
 
 //date with time
-$spreadsheet->getActiveSheet()
-			->setCellValue('A9',"Date/Time")
-			->setCellValue('B9',"Date Time")
-			->setCellValue('C9',Date::PHPToExcel($dateTimeNow));
+$sheet->setCellValue('A9',"Date/Time")
+		->setCellValue('B9',"Date Time")
+		->setCellValue('C9',Date::PHPToExcel($dateTimeNow));
 
 //set the cell format into a date
-$spreadsheet->getActiveSheet()
-			->getStyle('C9')
-			->getNumberFormat()
-			->setFormatCode(NumberFormat::FORMAT_DATE_DATETIME);
+$sheet->getStyle('C9')
+		->getNumberFormat()
+		->setFormatCode(NumberFormat::FORMAT_DATE_DATETIME);
 
 //only time
-$spreadsheet->getActiveSheet()
-			->setCellValue('A10',"Date/Time")
-			->setCellValue('B10',"Only Time")
-			->setCellValue('C10',Date::PHPToExcel($dateTimeNow));
+$sheet->setCellValue('A10',"Date/Time")
+		->setCellValue('B10',"Only Time")
+		->setCellValue('C10',Date::PHPToExcel($dateTimeNow));
 
 //set the cell format into a date
-$spreadsheet->getActiveSheet()
-			->getStyle('C10')
-			->getNumberFormat()
-			->setFormatCode(NumberFormat::FORMAT_DATE_TIME4);
+$sheet->getStyle('C10')
+		->getNumberFormat()
+		->setFormatCode(NumberFormat::FORMAT_DATE_TIME4);
 
 //rich text
-$spreadsheet->getActiveSheet()
-			->setCellValue('A11',"Rich text");
+$sheet->setCellValue('A11',"Rich text");
 
 $richText = new RichText();
 $richText->createText('normal text ');
@@ -133,30 +117,26 @@ $redText = $richText->createTextRun('red text');
 $redText->getFont()->setColor( new Color( Color::COLOR_RED ) );
 
 $richText->createText(' normal text again');
-$spreadsheet->getActiveSheet()->getCell('C11')->setValue($richText);
+$sheet->getCell('C11')->setValue($richText);
 
 //hyperlink
-$spreadsheet->getActiveSheet()
-			->setCellValue('A12',"Hyperlink")
-			->setCellValue('B12',"Cell Hyperlink")
-			->setCellValue('C12',"Visit Gemul's Channel");
+$sheet->setCellValue('A12',"Hyperlink")
+		->setCellValue('B12',"Cell Hyperlink")
+		->setCellValue('C12',"Visit Gemul's Channel");
 
 //set the cell as hyperlink
-$spreadsheet->getActiveSheet()
-			->getCell('C12')
-			->getHyperlink()
-			->setUrl('https://www.youtube.com')
-			->setTooltip('Ir a youtube');
+$sheet->getCell('C12')
+		->getHyperlink()
+		->setUrl('https://www.youtube.com')
+		->setTooltip('Ir a youtube');
 
 //hyperlink with formula
-$spreadsheet->getActiveSheet()
-			->setCellValue('A13',"Hyperlink")
-			->setCellValue('B13',"Formula Hyperlink")
-			->setCellValue('C13',"=HYPERLINK(\"https://www.facebook.com\",\"Ir a facebook\")");
+$sheet->setCellValue('A13',"Hyperlink")
+		->setCellValue('B13',"Formula Hyperlink")
+		->setCellValue('C13',"=HYPERLINK(\"https://www.facebook.com\",\"Ir a facebook\")");
 
 //change worksheet name
-$spreadsheet->getActiveSheet()
-			->setTitle('Phpspreadsheet');
+$sheet->setTitle('Phpspreadsheet');
 
 //crea el archivo excel con su nombre y lo guarda en el servidor
 $writer = new Xlsx($spreadsheet);
