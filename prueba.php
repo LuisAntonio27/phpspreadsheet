d5f9119bee02ff1b6d8c7693302b40b4a62410b9
Luis Antonio <luisantonio27@live.com.mx>
2020-03-14 13:41:43 -0600

    prueba sencilla para usar spreadsheet

---
+++ prueba.php
@@ -0,0 +1,20 @@
+<?php
+
+//llama el autoload
+require 'vendor/autoload.php';
+
+//carga la clase spreadsheet usando namespace
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+//llama la clase xlsx writes para hacer un archivo de excel
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+// crea un objeto spreadsheet
+$spreadsheet = new Spreadsheet();
+//obtiene la hoja actual
+$sheet = $spreadsheet->getActiveSheet();
+//configura el valor de la celda A1 en Hello World
+$sheet->setCellValue('A1', 'Hello World !');
+
+//crea el archivo excel con su nombre y lo guarda en el servidor
+$writer = new Xlsx($spreadsheet);
+$writer->save('hello world.xlsx');
